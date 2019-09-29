bfcb192ed871a45e6ed8ea3de2335f95047b1429
Mikeyouf <mik_anarchy@hotmail.com>
2019-09-29 21:46:28 +0200

    css blog prestation

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ module.exports = {
   siteTitle: 'La ferme du Gapeau', // Navigation and Site Title
   siteTitleAlt: 'Esat la ferme du Gapeau', // Alternative Site title for SEO
   siteTitleManifest: 'FDG',
-  siteUrl: 'https://minimal-blog.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://demo-styled-component.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'fr', // Language Tag on <html> element
-  siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
+  siteHeadline: 'Site de démo pour un cours', // Headline for schema.org JSONLD
   siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
-  author: 'LekoArts', // Author for schemaORGJSONLD
+  siteDescription: 'Cours sur l\'utilisation de styled component', // Your site description
+  author: 'Mik@el', // Author for schemaORGJSONLD
   siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
@@ -21,6 +21,6 @@ module.exports = {
 
   // Manifest and Progress color
   // See: https://developers.google.com/web/fundamentals/web-app-manifest/
-  themeColor: '#3498DB',
+  themeColor: '#53AD53',
   backgroundColor: '#2b2e3c',
 }
--- src/components/blocPrestation/BlocPrestation.js
+++ src/components/blocPrestation/BlocPrestation.js
@@ -1,10 +1,135 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const BlocPrestation = () => (
+import bckImage1 from '../../img/fdg5.jpg'
+import bckImage2 from '../../img/fdg4.jpg'
+import bckImage3 from '../../img/fdg6.jpg'
+// import bckImage from '../../img/fdg5.jpg'
+
+const Bloc = styled.div`
+    background-color: #000;
+    width: 100%;
+
+    :nth-child(1) {
+        background-color: #000000;
+        display: flex;
+        align-items: center;
+    }
+
+
+`
+
+const BlocImage = styled.div`
+    background-color: #000;
+    width: 100%;
+    height: 480px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-content: space-evenly;
+
+    div {
+        width: 240px;
+        height: 160px;
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    p {
+        color: white;
+        text-align: center;
+        background-color: rgba(0, 0, 0, 0.7);
+        padding: 1em 0;
+    }
+
+    .visible {
+        opacity: 1;
+        transform: translateY(-120px);
+        transition: all 0.4s ease-in;
+    }
+
+    .hidden {
+        opacity: 0;
+        transition: all 0.4s ease-in;
+    }
+
+`
+
+const BlocTexte = styled.div`
+    color: #fff;
+    background-color: #000;
+    text-align: right;
+    font-family: 'poppins';
+    padding: 1em;
+
+    h2 {
+        color: ${props => props.theme.colors.primary};
+        font-family: 'great vibes';
+        margin: 0;
+    }
+
+    h3 {
+        color: #fff;
+        text-transform: uppercase;
+        font-family: 'josefin sans';
+        margin-top: -15px;
+    }
+
+    p {
+        color: #808080;
+        font-size: 15px;
+        line-height: 27px;
+        text-align: justify;
+    }
+`
+
+
+
+const BlocPrestation = () => {
+    const [ isHover, setIsHover ] = useState(1)
+    const changeHover = id => {
+        setIsHover(id)
+    }
+
+    return (
     <>
-        test
+        <Bloc>
+            <BlocTexte>
+                <h2>Découvrez</h2>
+                <h3>nos prestations</h3>
+                <p>
+                    Notre établissement propose différentes prestations de qualité tout au long de l'année.
+                    Le pôle vert est divisé en deux secteurs diffénts, le maraichage et l'entretien des espaces
+                    vert.
+                    Le pôle restauration, propose des rapas le midi dans un cadre charmant. Il propose également
+                    la création de cocktails et la location de salle de réunion avec un service de repas.
+                </p>
+            </BlocTexte>
+        </Bloc>
+        <BlocImage>
+            <div onMouseEnter={() => changeHover(1)} onMouseLeave={() => setIsHover(false)} id="1">
+                <img src={bckImage1} alt="ferme du gapeau"/>
+                <p className={isHover === 1 ? 'visible' : 'hidden'} >Pôle travail</p>
+            </div>
+            <div onMouseEnter={() => changeHover(2)} onMouseLeave={() => setIsHover(false)} naidme="2">
+                <img src={bckImage2} alt="ferme du gapeau"/>
+                <p className={isHover === 2 ? 'visible' : 'hidden'}>Pôle restauration</p>
+            </div>
+            <div onMouseEnter={() => changeHover(3)} onMouseLeave={() => setIsHover(false)} id="3">
+                <img src={bckImage3} alt="ferme du gapeau"/>
+                <p className={isHover === 3 ? 'visible' : 'hidden'}>Pôle social</p>
+            </div>
+            <div onMouseEnter={() => changeHover(4)} onMouseLeave={() => setIsHover(false)} id="4">
+                <img src={bckImage1} alt="ferme du gapeau"/>
+                <p className={isHover === 4 ? 'visible' : 'hidden'}>Pôle administratif</p>
+            </div>
+        </BlocImage>
     </>
-)
+    )
+}
 
 export default BlocPrestation;
