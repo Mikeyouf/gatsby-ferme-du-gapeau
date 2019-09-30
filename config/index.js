c3a50abcdb859b9f556240eb65f982e29c5689b3
Mikeyouf <mik_anarchy@hotmail.com>
2019-09-30 13:58:13 +0200

    change logo banner

--- config/index.js
+++ config/index.js
@@ -7,11 +7,11 @@ module.exports = {
   siteUrl: 'https://demo-styled-component.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'fr', // Language Tag on <html> element
   siteHeadline: 'Site de démo pour un cours', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/logo2.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Cours sur l\'utilisation de styled component', // Your site description
   author: 'Mik@el', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
+  siteLogo: '/logo2.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@minimal', // Twitter Username - Optional
